67dd793c0d3ae6effa5c2f916df4a9dc4b10ebb0
SanaGo108 <oksana135@mail.ru>
2024-04-21 00:23:21 +0300

    add new class

--- main.py
+++ main.py
@@ -20,19 +20,59 @@
 #метод для получения цены товара по его названию. Если товар отсутствует, возвращайте None.
 #метод для обновления цены товара.
 
+#2. Создай несколько объектов класса Store:
+#Создай не менее трех различных магазинов с разными названиями, адресами и добавь в каждый
+# из них несколько товаров.
+
+#3. Протестировать методы:
+#Выбери один из созданных магазинов и протестируй все его методы: добавь товар,
+# обнови цену, убери товар и запрашивай цену.
+#В поле для ответа загрузи ссылку на GitHub-репозиторий, содержащий код проекта с
+# реализацией задания.
+
 class Store():
-    def __init__(self, name, address, items):
+    def __init__(self, name, address):
         self.name = name
         self.address = address
-        self.items = items
+        self.items = {}
 
-    items = {
-        'яблоки': 130,
-        'бананы': 250,
-        'мандарины': 200,
-        'картошка': 140,
-        'помидоры': 250
-        'лук': 50
-    }
+    def add_item(self, item_name, price):
+        self.items[item_name] = price
 
-def __init__():
+    def remove_item(self, item_name):
+        if item_name in self.items:
+            del self.items[item_name]
+
+    def get_price(self, item_name):
+        return self.items.get(item_name, None)
+
+    def update_price(self, item_name, new_price):
+        if item_name in self.items:
+            self.items[item_name] = new_price
+
+store1 = Store(name="Десяточка", address="Москва, ул. Академика Королева, 12")
+store2 = Store(name="Техника", address="Санкт-Петербург, ул. Глинянная, 1")
+store3 = Store(name="Все для дачи", address="Ростов на Дону, ул. Речная, 5")
+
+store1.add_item("яблоки", 180)
+store1.add_item("молоко", 75)
+store1.add_item("гречка", 140)
+store1.add_item("печенье", 98)
+store2.add_item("Пылесос", 15999)
+store2.add_item("утюг", 2900)
+store2.add_item("миксер", 6999)
+store2.add_item("мультиварка", 25000)
+store3.add_item("газонокосилка", 38999)
+store3.add_item("шланг садовый", 1999)
+store3.add_item("грабли", 2500)
+store3.add_item("тачка", 6999)
+
+store1.add_item("картошка", 199)
+store1.add_item("масло", 240)
+store1.update_price("яблоки", 165)
+removed_price = store1.get_price("молоко")
+store1.remove_item("молоко")
+new_price_apples = store1.get_price("яблоки")
+new_price_oranges = store1.get_price("картошка")
+nonexistent_price = store1.get_price("молоко")
+
